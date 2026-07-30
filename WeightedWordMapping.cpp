071b6a5bdf7458e3fa62da2e0cbffe3be4e4ffa4
Deepanshugupta2001 <deepanshugupta.dg.2001@gmail.com>
2026-07-30 23:12:27 +0530

    SOlved

---
+++ WeightedWordMapping.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        int n=words.size();
+        string ans;
+        string f="abcdefghijklmnopqrstuvwxyz";
+        reverse(f.begin(),f.end());
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=0;j<words[i].size();j++){
+                sum+=weights[words[i][j]-'a'];
+            }
+            sum=sum%26;
+            ans+=f[sum];
+        }
+        return ans;
+    }
+};
