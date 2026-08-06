1dcf1b78316ae6975655b5489aa6d05aa7f39ed1
Deepanshugupta2001 <deepanshugupta.dg.2001@gmail.com>
2026-08-06 22:25:04 +0530

    Solved

---
+++ MinimumElementAfterReplacementWithDigitSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int sum=0;
+            while(nums[i]){
+                sum+=nums[i]%10;
+                nums[i]/=10;
+            }
+            nums[i]=sum;
+        }
+        sort(nums.begin(),nums.end());
+        return nums[0];
+    }
+};
