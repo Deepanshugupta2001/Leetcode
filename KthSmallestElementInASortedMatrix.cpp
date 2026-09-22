9fc293ad6bfa01e8b8031f9b417aaa40c1705c69
Deepanshugupta2001 <deepanshugupta.dg.2001@gmail.com>
2026-09-22 22:20:55 +0530

    Solved

---
+++ KthSmallestElementInASortedMatrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int l) {
+        int n=matrix.size();
+        vector<int> a(n*n);
+        int k=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[k]=matrix[i][j];
+                k++;
+            }
+        }
+        sort(a.begin(),a.end());
+        return a[l-1];
+    }
+};
