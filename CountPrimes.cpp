d06c2e8094a6c0d6507dc25aa4bebc401a065c2e
Deepanshugupta2001 <deepanshugupta.dg.2001@gmail.com>
2026-09-25 19:55:50 +0530

    Solved

---
+++ CountPrimes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2)
+            return 0;
+
+        vector<bool> isPrime(n, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        // All even numbers except 2 are not prime
+        for (int i = 4; i < n; i += 2)
+            isPrime[i] = false;
+
+        for (int i = 3; i * i < n; i += 2) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += 2 * i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int cnt = 1;  // 2 is prime
+
+        for (int i = 3; i < n; i += 2) {
+            if (isPrime[i])
+                cnt++;
+        }
+
+        return cnt;
+    }
+};
